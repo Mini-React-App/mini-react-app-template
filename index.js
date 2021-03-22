c362cb1d6c142d2dc0c361010a37af1095e32d65
sh.gazgireev <shakhbulatgaz@gmail.com>
2021-03-22 11:38:36 +0300

    Initial commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+	<>
+		<h1>Welcome to your Mini React App</h1>
+		<h2>The most minimalistic React project template ever!</h2>
+	</>
+);
+
+ReactDOM.render(<App/>, document.getElementById("root"));
